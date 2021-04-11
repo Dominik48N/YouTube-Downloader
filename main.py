802e2796d3b137bd65619dbbe51950c5bdb38d15
Dominik <admin@dominik48n.de>
2021-04-11 21:09:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,22 @@ from pytube import YouTube
 import os
 
 
-def downloadYouTubeVideo(videoUrl, downloadPath):
+def downloadYouTubeVideo():
     try:
-        yt = YouTube(videoUrl)
+        yt = YouTube(video_url)
         yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
 
-        if not os.path.exists(downloadPath):
-            os.makedirs(downloadPath)
+        if not os.path.exists(download_path):
+            os.makedirs(download_path)
 
-        yt.download(downloadPath)
+        yt.download(download_path)
         print("The video is downloaded!")
     except:
         print("There has been an error. Presumably the video was not found.")
 
 
 if __name__ == "__main__":
-    videoUrl = input("What is the YouTube video URL? ")
-    downloadPath = input("Please enter the path in which the downloaded YouTube video should be played. ")
+    video_url = input("What is the YouTube video URL? ")
+    download_path = input("Please enter the path in which the downloaded YouTube video should be played. ")
 
-    downloadYouTubeVideo(videoUrl, downloadPath)
+    downloadYouTubeVideo()
